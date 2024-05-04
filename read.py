b0b508927bd84d8c352375f4161ce49032bb22d6
Suman Paudel <dastonsuman1997@gmail.com>
2024-05-04 14:48:34 +0545

    reading images and videos

--- read.py
+++ read.py
@@ -5,7 +5,19 @@ images = r"Resources/Photos/"
 
 img = cv.imread(f'{images}cat.jpg')
 
-cv.imshow(winname='Cat',mat=img)
+# cv.imshow(winname='Cat',mat=img)
 cv.waitKey(0)
 
-vid = cv.imread(f'{vidoes}dog.mp4')
+
+capture = cv.VideoCapture(f'{vidoes}dog.mp4')
+
+while True:
+    isTrue, frame = capture.read()
+    
+    cv.imshow('Dog',frame)
+    
+    if cv.waitKey(0) and 0xFF==ord('d'):
+        break
+    
+capture.release()
+cv.destroyAllWindows()

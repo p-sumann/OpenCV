fd3a4c365e0a6047f31d1ed1b7d42f062385cde7
Suman Paudel <dastonsuman1997@gmail.com>
2024-05-04 16:01:19 +0545

    drawing shapes

--- draw.py
+++ draw.py
@@ -15,6 +15,13 @@ cv.rectangle(zeros, (0,0),(img.shape[1]//2,img.shape[0]//2),(0,0,255), thickness
 
 # 3. draw a circle 
 cv.circle(zeros, (img.shape[1]//2,img.shape[0]//2), 40, (0,250,0),thickness=cv.FILLED)
+# cv.imshow(winname='Blank', mat=zeros)
+
+# 4. draw a line
+cv.line(zeros, (255,255), (1000,1000),5,2)
+
+# 5. write a text in an image
+cv.putText(zeros, 'Suman is Learning OpenCV',  (255,255), fontFace=cv.FONT_HERSHEY_TRIPLEX, fontScale=.5, color=6)
 cv.imshow(winname='Blank', mat=zeros)
 
 cv.waitKey(0)
